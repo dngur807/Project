27214adbd3c93b7de2e0b80535afb56d8ae3dff2
정우혁 <dngur807@naver.com>
2018-10-07 13:46:24 +0900

    develop 에서 commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Project
 PHP 연습
+
+Feature Branch 기반으로 만듬
+
+develop 체크 아웃
+
+
---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JWH
+ * Date: 2018-10-07
+ * Time: 오후 1:21
+ */
+
+echo phpinfo();
+
+echo "develop 체크 아웃";  
